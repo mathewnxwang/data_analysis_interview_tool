ab3200df175aca72831d7f4cf8ce6f7d3a41bbd8
Mathew Wang <mathewnxwang@gmail.com>
2024-08-09 20:05:35 -0400

    updating CodeExecutor

--- code_executor.py
+++ code_executor.py
@@ -1,13 +1,20 @@
 import pandas as pd
 
 class CodeExecutor():
-    def execute_code(self, df: pd.DataFrame, code: str):
+    def execute_code(self, df: pd.DataFrame, input_code: str):
+        print(type(df))
         local_vars = {'df': df}
+        code_prefix = """import pandas as pd\nresult = """
         try:
-            exec(code, {}, local_vars)
-        except:
-            return "Error in code execution"
-
+            exec(code_prefix + input_code, {}, local_vars)
+        except Exception as e:
+            return f"Error in code execution: {e}\nCompiled code: {code_prefix + input_code}"
+        
         execution_result = local_vars.get('result', None)
         print(execution_result)
+        print(type(execution_result))
+
+        if isinstance(execution_result, pd.DataFrame):
+            return execution_result.to_html()
+
         return execution_result
